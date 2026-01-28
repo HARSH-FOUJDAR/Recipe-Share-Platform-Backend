3174fc0cc6e70c20b603e6178c05d80119b56b33
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-28 19:45:15 +0530

    update

--- controllers/recipe.controller.js
+++ controllers/recipe.controller.js
@@ -1,5 +1,6 @@
 const RecipeModel = require("../models/Recipe.model");
 
+// CREATE RECIPE
 exports.createRecipe = async (req, res) => {
   try {
     const {
@@ -21,35 +22,39 @@ exports.createRecipe = async (req, res) => {
       !cookTime ||
       !servings
     ) {
-      return res.status(400).json({ message: "Required Field  are Missing" });
+      return res.status(400).json({ message: "Required fields are missing" });
     }
+
     const newRecipe = new RecipeModel({
       title,
       ingredients,
       instructions,
       steps,
       cookTime,
-      photos,
       servings,
+      photos,
       videoTutorial,
       createdBy: req.user._id,
     });
 
     await newRecipe.save();
-    res
-      .status(201)
-      .json({ message: "Recipe created successfully", recipe: newRecipe });
+
+    res.status(201).json({
+      message: "Recipe created successfully",
+      recipe: newRecipe,
+    });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 };
 
+// GET ALL RECIPES
 exports.getAllRecipe = async (req, res) => {
   try {
     const recipes = await RecipeModel.find().populate(
       "createdBy",
-      "username email",
+      "username email"
     );
     res.json(recipes);
   } catch (error) {
@@ -57,30 +62,25 @@ exports.getAllRecipe = async (req, res) => {
   }
 };
 
+// GET RECIPE BY ID
 exports.getRecipeById = async (req, res) => {
   try {
     const recipe = await RecipeModel.findById(req.params.id).populate(
       "createdBy",
-      "username email",
+      "username email"
     );
+
     if (!recipe) {
       return res.status(404).json({ message: "Recipe not found" });
     }
+
     res.json(recipe);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
 };
 
-exports.deleteRecipeById = async (req, res) => {
-  try {
-    await req.recipe.deleteOne();
-    res.json({ message: "Recipe deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-};
-
+// UPDATE RECIPE
 exports.updateById = async (req, res) => {
   try {
     const allowedFields = [
@@ -93,93 +93,22 @@ exports.updateById = async (req, res) => {
       "servings",
       "videoTutorial",
     ];
+
     allowedFields.forEach((field) => {
       if (req.body[field] !== undefined) {
         req.recipe[field] = req.body[field];
       }
     });
+
     await req.recipe.save();
-    res.json({
-      message: "Recpies update succesffully",
-    });
+
+    res.json({ message: "Recipe updated successfully" });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
 };
 
-
-exports.createRecipe = async (req, res) => {
-  try {
-    const {
-      title,
-      ingredients,
-      instructions,
-      steps,
-      cookTime,
-      servings,
-      photos,
-      videoTutorial,
-    } = req.body;
-
-    if (
-      !title ||
-      !ingredients ||
-      !instructions ||
-      !steps ||
-      !cookTime ||
-      !servings
-    ) {
-      return res.status(400).json({ message: "Required Field  are Missing" });
-    }
-    const newRecipe = new RecipeModel({
-      title,
-      ingredients,
-      instructions,
-      steps,
-      cookTime,
-      photos,
-      servings,
-      videoTutorial,
-      createdBy: req.user._id,
-    });
-
-    await newRecipe.save();
-    res
-      .status(201)
-      .json({ message: "Recipe created successfully", recipe: newRecipe });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Server error" });
-  }
-};
-
-exports.getAllRecipe = async (req, res) => {
-  try {
-    const recipes = await RecipeModel.find().populate(
-      "createdBy",
-      "username email",
-    );
-    res.json(recipes);
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-};
-
-exports.getRecipeById = async (req, res) => {
-  try {
-    const recipe = await RecipeModel.findById(req.params.id).populate(
-      "createdBy",
-      "username email",
-    );
-    if (!recipe) {
-      return res.status(404).json({ message: "Recipe not found" });
-    }
-    res.json(recipe);
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-};
-
+// DELETE RECIPE
 exports.deleteRecipeById = async (req, res) => {
   try {
     await req.recipe.deleteOne();
@@ -189,82 +118,37 @@ exports.deleteRecipeById = async (req, res) => {
   }
 };
 
-exports.updateById = async (req, res) => {
-  try {
-    const allowedFields = [
-      "title",
-      "ingredients",
-      "instructions",
-      "steps",
-      "cookTime",
-      "photos",
-      "servings",
-      "videoTutorial",
-    ];
-    allowedFields.forEach((field) => {
-      if (req.body[field] !== undefined) {
-        req.recipe[field] = req.body[field];
-      }
-    });
-    await req.recipe.save();
-    res.json({
-      message: "Recpies update succesffully",
-    });
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-};
-
-exports.tooglelike = async (req, res) => {
+// TOGGLE LIKE
+exports.toggleLike = async (req, res) => {
   const { id } = req.params;
   const userId = req.user._id;
 
   try {
     const recipe = await RecipeModel.findById(id);
     if (!recipe) {
-      return res.status(404).json({
-        message: "Recipe not Found",
-      });
+      return res.status(404).json({ message: "Recipe not found" });
     }
+
     const index = recipe.like.indexOf(userId);
+
     if (index === -1) {
       recipe.like.push(userId);
     } else {
       recipe.like.splice(index, 1);
     }
+
     await recipe.save();
+
     res.json({
       likeCount: recipe.like.length,
       liked: index === -1,
     });
-  } catch (err) {
-    res.status(500).json({
-      message: "Server Error",
-    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
   }
 };
 
-exports.shareRecipe = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const recipe = await RecipeModel.findById(id);
-    if (!recipe) {
-      return res.status(404).json({
-        message: "Recipe Not found",
-      });
-    }
-    recipe.share = (recipe.share || 0) + 1;
-    await recipe.save();
-    res.json({
-      sharCount: recipe.share,
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: "Server Error",
-    });
-  }
-};
-
+// SHARE RECIPE
 exports.shareRecipe = async (req, res) => {
   const { id } = req.params;
 
@@ -274,13 +158,13 @@ exports.shareRecipe = async (req, res) => {
       return res.status(404).json({ message: "Recipe not found" });
     }
 
-    recipe.shares += 1;
+    recipe.share += 1;
     await recipe.save();
 
     res.json({
-      sharesCount: recipe.shares,
+      shareCount: recipe.share,
     });
-  } catch (err) {
-    res.status(500).json({ message: "Server Error" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
   }
 };
