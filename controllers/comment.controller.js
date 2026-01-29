6aace3af8894a6c75c604b1b690bf2cb5221c2cf
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-29 09:28:51 +0530

    update

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,18 +1,30 @@
 const CommentModel = require("../models/Comment.model");
-const Comment = require("../models/Comment.model");
 exports.addComment = async (req, res) => {
   try {
     const { recipeId, text } = req.body;
+
+    //  Comment create karo
     const comment = await CommentModel.create({
       recipe: recipeId,
       user: req.user._id,
       text,
     });
-    res.json({ message: "Comment add Successfylly", comment });
+
+    // Populate user data
+    const populatedComment = await CommentModel.findById(comment._id).populate(
+      "user",
+      "username",
+    );
+
+    // Send response
+    res.json({
+      message: "Comment added successfully",
+      comment: populatedComment,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: "Comment not Created",
+      message: "Comment not created",
     });
   }
 };
