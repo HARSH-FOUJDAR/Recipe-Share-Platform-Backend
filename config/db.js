7453f809d2e7d6e1101069f3cd2ffb92ae618dc4
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-30 16:46:19 +0530

    sdsd

--- config/db.js
+++ config/db.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
 
+let isConnected = false; 
+
 const Databse = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("Using existing database connection");
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.MongoDB_URI);
+    const db = await mongoose.connect(process.env.MongoDB_URI, {
+      serverSelectionTimeoutMS: 3000,
+    });
+
+    isConnected = db.connections[0].readyState;
     console.log("Database is connected");
   } catch (err) {
-    console.log(err, "Dtabase in not connected");
+    console.log("Database connection error:", err.message);
+  
   }
 };
 
