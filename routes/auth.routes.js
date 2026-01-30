d1844dddc8693a4b7afda472f5f1499ac1567c05
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-30 22:18:21 +0530

    dd

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const path = require("path");
 const Authcontrollers = require("../controllers/auth.controller");
 const authenticate = require("../middleware/auth.middleware");
-const multer = require("multer");
 
 router.post("/register", Authcontrollers.Registerpage);
 router.post("/login", Authcontrollers.Loginpage);
@@ -11,15 +10,7 @@ router.post("/forgotpassword", Authcontrollers.forgotPassword);
 router.post("/reset-password/:token", Authcontrollers.resetPassword);
 router.get("/me", authenticate, Authcontrollers.getMe);
 
-const avtarStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/uploads/avatars");
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    cb(null, `${Date.now()}_${Math.random()}${ext}`);
-  },
-});
+
 
 router.get("/profile", authenticate, Authcontrollers.userprofile);
 router.put("/updateprofile", authenticate, Authcontrollers.updateProfile);
