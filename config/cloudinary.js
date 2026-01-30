6e14e18db67f1b328ed146f13b2b8345ae71b107
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-30 12:57:22 +0530

    ww

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,26 @@
-const cloudinary = require("cloudinary").v2;
+const { v2: cloudinary } = require("cloudinary");
+const multer = require("multer");
+const streamifier = require("streamifier");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports = cloudinary;
+const upload = multer({ storage: multer.memoryStorage() });
+
+exports.uploadProfileImage = (req, res, next) => {
+  if (!req.file) return next();
+
+  const stream = cloudinary.uploader.upload_stream(
+    { folder: "profiles" },
+    (err, result) => {
+      if (err) return next(err);
+      req.file.path = result.secure_url; // <-- assign Cloudinary URL
+      next();
+    }
+  );
+
+  streamifier.createReadStream(req.file.buffer).pipe(stream);
+};
