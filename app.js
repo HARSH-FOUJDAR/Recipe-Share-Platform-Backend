354645458e4198ffb7a90151d967d411654bc6c0
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-30 21:26:51 +0530

    sss

--- app.js
+++ app.js
@@ -47,4 +47,8 @@ app.get("/", (req, res) => {
 
 Databse();
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
