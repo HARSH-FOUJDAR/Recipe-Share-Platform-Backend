b0e70f0ba3ae1331ab07d2109b16a410c8da5c05
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-30 13:29:12 +0530

    ww

--- middleware/upload.middleware.js
+++ middleware/upload.middleware.js
@@ -1,8 +1,7 @@
 const { v2: cloudinary } = require("cloudinary");
-const { CloudinaryStorage } = require("multer-storage-cloudinary"); // <-- Spelling check karein
-const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer"); 
 
-// Configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Authcontrollers = require("../controllers/auth.controller");
 const authenticate = require("../middleware/auth.middleware");
-
-const upload = require("../middleware/upload.middleware");
+const upload = require("../middleware/upload.middleware"); // <--- Check filename: upload.middleware.js
 
 router.post(
   "/register",
@@ -12,11 +11,8 @@ router.post(
 );
 router.post("/login", Authcontrollers.Loginpage);
 router.post("/forgotpassword", Authcontrollers.forgotPassword);
-
-//  Reset password using token
 router.post("/reset-password/:token", Authcontrollers.resetPassword);
 router.get("/me", authenticate, Authcontrollers.getMe);
-
 router.get("/profile", authenticate, Authcontrollers.userprofile);
 router.put(
   "/updateprofile",
